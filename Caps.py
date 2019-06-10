bbc70b73fb245a4b1f5b19b9ecacaf78fc5e60e2
Lenovo11 <51495563+Lenovo11@users.noreply.github.com>
2019-06-10 16:28:44 -0400

    Add files via upload

---
+++ Caps.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# ## Notebok for Capstone project
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
